18b301612105ec303e3db28edb06877096b5b0ec
Yugank Rai <raiyugank184@gmail.com>
2025-09-09 10:31:49 +0530

    index.html, script.js, style.css
    this is the frontend for home page or index page.

---
+++ script.js
@@ -0,0 +1,6 @@
+function loginAs(role) {
+  alert("Redirecting to " + role + " login/signup page...");
+  // Here you can redirect to actual login pages
+  // window.location.href = role.toLowerCase() + "-login.html";
+}
+
